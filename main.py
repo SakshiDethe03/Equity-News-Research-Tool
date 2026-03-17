14ecafabcd37523033fd5d6ac5b54d029b3d0eb9
Sakshi Dethe <sakshidethe07@gmail.com>
2026-03-17 22:12:55 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+import streamlit as st
+import time
+from langchain_openai import ChatOpenAI
+from langchain_classic.chains import RetrievalQAWithSourcesChain
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from key import api_key
+
+os.environ["OPENAI_API_KEY"] = api_key
+
+llm = ChatOpenAI(
+    model="nvidia/nemotron-3-super-120b-a12b:free",
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+    temperature=0.6
+)
+
+st.title("Equity News Research Tool 📈")
+st.sidebar.title("News Article URLs 🔗")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+
+main_placeholder = st.empty()
+
+# ================= PROCESSING =================
+if process_url_clicked:
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅")
+    data = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000,
+    )
+    main_placeholder.text("Text Splitting...Started...✅")
+    docs = text_splitter.split_documents(data)
+
+    embeddings = OpenAIEmbeddings(
+        api_key=api_key,
+        base_url="https://openrouter.ai/api/v1"
+    )
+
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Done...✅")
+    time.sleep(2)
+
+    vectorstore_openai.save_local("faiss_index")
+
+
+# ================= QUERY =================
+query = st.text_input("Enter your question about the news articles:")
+
+if query:
+    if os.path.exists("faiss_index"):
+
+        embeddings = OpenAIEmbeddings(
+            api_key=api_key,
+            base_url="https://openrouter.ai/api/v1"
+        )
+
+        vectorstore = FAISS.load_local(
+            "faiss_index",
+            embeddings,
+            allow_dangerous_deserialization=True
+        )
+
+        chain = RetrievalQAWithSourcesChain.from_llm(
+            llm=llm,
+            retriever=vectorstore.as_retriever(),
+        )
+
+        result = chain({"question": query}, return_only_outputs=True)
+
+        st.header("Answer:")
+        st.write(result['answer'])
+
+        sources = result.get("sources", "")
+        if sources:
+            st.subheader("Sources:")
+            sources_list = sources.split("\n")
+            for source in sources_list:
+                st.write(source)
